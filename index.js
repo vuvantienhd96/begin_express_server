95549e465eacdc419cc5251820bfb5cdd1f98c21
tienvuv1 <congtuocxauxa000@gmail.com>
2020-04-02 20:26:01 +0700

    begin with expressJS

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello world'));
+app.listen(port, () => console.log(`example app listening at port ${port}`));
